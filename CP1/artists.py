313a56a12ac8b0888815953d2edfee69556e2b7a
João Raimundo <joao.raimundo.1982@gmail.com>
2021-10-08 21:14:08 +0100

    update new streams

--- CP1/artists.py
+++ CP1/artists.py
@@ -3,61 +3,40 @@ import yaml
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
+path = 'project_datasets/global/'
 
 with open("spotify_api_credentials.yaml","r") as yamlfile:
     data = yaml.load(yamlfile, Loader=yaml.FullLoader)
     client_id =data['client_id']
     secret = data['secret']
 
-
-def add_value_to_df(df, id, column, value):
-    index = df.loc[df['artist_id']==id].index
-    df.loc[index[0], column] = value
-    return
-
-def extract_elements(arr):
-    str = ''
-    for element in arr:
-        str += element + ', ' 
-    return str[:-1]
-
-def convert_to_array(val):
-    val = val.split(", ")
-    val=val[0:len(val)-1]
-    print(val)
-    return val
-    
-
 client_credentials_manager = SpotifyClientCredentials(client_id, secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 # create dataframe from the csv file
-tracks_df = pd.read_csv("tracks.csv",delimiter=',',encoding="latin1")
+tracks_df = pd.read_csv(path + "global_tracks_2017_2020.csv", delimiter=',', na_values=["", None], na_filter=True)
+tracks_df=pd.DataFrame(tracks_df.assign(artist_id=tracks_df.artist_id.str.split(",")).explode('artist_id'))
 
-artists_df = pd.DataFrame()
+length = len(tracks_df.artist_id.unique())
+counter = 0
+last_pct = 0
 
-artists_ids = []
-for element in tracks_df.artist_id:
-    for id in element.split(', '):
-        artists_ids.append(id)
-
-artists_df['artist_id'] = artists_ids
-
-#artists_df = artists_df.drop_duplicates(subset="artist_id", keep='first')
-
-
-artists_df['followers'] = ""
-artists_df['genres'] = ""
-artists_df['popularity'] = ""
-artists_df['name'] = ""
-
-
-for id in artists_df['artist_id']:
+artists = []
+for id in tracks_df.artist_id.unique():   
     artist_info = sp.artist(id)
-    type(artist_info['genres'])
-    add_value_to_df(artists_df, id, "followers", int(artist_info["followers"]["total"]))
-    add_value_to_df(artists_df, id, "popularity", int(artist_info["popularity"]))
-    add_value_to_df(artists_df, id, "genres", ', '.join(artist_info['genres']))
-    add_value_to_df(artists_df, id, "name", artist_info["name"])
 
-artists_df.to_csv('artists.csv', index=False, header=True)
+    name = artist_info["name"]
+    genres = ','.join(artist_info['genres'])
+    followers =  int(artist_info["followers"]["total"])
+    popularity = int(artist_info["popularity"])
+
+    artists.append([id, name, genres, followers, popularity])
+
+    counter += 1
+    pct = round(counter/length*100)
+    if pct != last_pct:
+        print(f'\x1b[1A\x1b[2K{counter}/{length} => {pct}%')
+    last_pct = pct
+
+artists_df = pd.DataFrame(artists, columns= ["artist_id","name", "genres", "followers", "popularity"])
+artists_df.to_csv(path + 'global_artists_2017_2020.csv', index=False, header=True)
--- CP1/tracks.py
+++ CP1/tracks.py
@@ -9,62 +9,57 @@ with open("spotify_api_credentials.yaml","r") as yamlfile:
     client_id =data['client_id']
     secret = data['secret']
 
-def add_value_to_df(df, column_value, column, value):
-    index = df.loc[df['track_id']==column_value].index
-    df.loc[index[0], column] = value
-
-client_credentials_manager = SpotifyClientCredentials(client_id, secret)
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-
-# create dataframe from the csv file
-spcharts_df = pd.read_csv("global_streams_2017_2020.csv",delimiter=',',encoding="latin1")
-
-# create new DF to store all the tracks info
-tracks_df = pd.DataFrame()
-tracks_df['track_id'] = spcharts_df.song_id.unique()
-
-# add columns
-
-tracks_df['popularity'] = ""
-tracks_df['explicit'] = ""
-tracks_df['name'] = ""
-tracks_df['popularity'] = ""
-tracks_df['artist_name'] = ""
-tracks_df['artist_id'] = ""
-
-id = "3AEZUABDXNtecAOSC1qTfo"
-#id = "51Ss1yLa32T4zi3C82QkZF"
-features_columns = sp.audio_features([id])
-features = []
-
-for k in features_columns[0]:
-    features.append(k)
-    tracks_df[k] = ""
-
-keys = tracks_df.columns.drop(['track_id',*features])
-
 def extract_elements(arr,key):
     str = ''
     for element in arr:
         str += element[key] + ',' 
     return str[:-1]
-    
+
+client_credentials_manager = SpotifyClientCredentials(client_id, secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+# create dataframe from the csv file
+spcharts_df = pd.read_csv("project_datasets/global/global_streams_2017_2020.csv",delimiter=',',encoding="latin1")
+
+track_ids = spcharts_df.track_id.unique()
+tracks = []
+ 
 counter = 0
-for id in tracks_df['track_id']:
+last_pct = 0
+length = len(track_ids)
+for id in track_ids:
     counter+=1
     track_info = sp.track(id,'pt')
     audio_features = sp.audio_features([id])[0]
 
-    if track_info != None:
-        for k in keys:
-            if "artist" in k:
-                add_value_to_df(tracks_df, id, k, extract_elements(track_info["artists"],k.split("_")[1]))
-            else:
-                add_value_to_df(tracks_df, id, k, track_info[k])
+    if track_info != None and audio_features != None:
 
-    if audio_features != None:
-        for f in features:
-            add_value_to_df(tracks_df, id, f, audio_features[f])
-    
-tracks_df.drop(['track_href','analysis_url','type','uri'], axis=1, inplace=True)
-tracks_df.to_csv('global_tracks.csv', index=False, header=True)
+        name = track_info['name']
+        artist_id = extract_elements(track_info["artists"],'id') 
+        artist_name = extract_elements(track_info["artists"], 'name')
+        popularity = track_info['popularity']
+        explicit = track_info['explicit']
+        duration_ms = track_info['duration_ms']
+
+        danceability = audio_features['danceability']
+        energy = audio_features['energy']
+        key = audio_features['key']
+        loudness = audio_features['loudness']
+        mode = audio_features['mode']
+        speechiness = audio_features['speechiness']
+        acousticness = audio_features['acousticness']
+        instrumentalness = audio_features['instrumentalness']
+        liveness = audio_features['liveness']
+        valence = audio_features['valence']
+        tempo = audio_features['tempo']
+        time_signature = audio_features['time_signature']
+
+    tracks.append([id,name,artist_id,artist_name, popularity, explicit,duration_ms, danceability,energy,key,loudness,mode,speechiness,acousticness,instrumentalness,liveness,valence,tempo,time_signature])
+    pct = round(counter/length*100)
+    if pct != last_pct:
+        print(f'\x1b[1A\x1b[2K{counter}/{length} => {pct}%')
+    last_pct = pct
+
+
+tracks_df = pd.DataFrame(tracks, columns= ["track_id","name","artist_id", "artist_name", "popularity", "explicit","duration_ms","danceability","energy","key","loudness","mode","speechiness","acousticness","instrumentalness","liveness","valence","tempo","time_signature"])
+tracks_df.to_csv('project_datasets/global/global_tracks_2017_2020.csv', index=False, header=True)
